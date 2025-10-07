74675c3db8cdc339e8bddb6eddc27a0042d81a47
saniyafatima07 <saniyafatima200631@gmail.com>
2025-10-07 22:43:58 +0530

    Connect with google sheets

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ document.getElementById("officialForm").addEventListener("submit", function(e){
         document.getElementById("officialForm").reset();
     }, 1000);
 });
+
+const form = document.getElementById("officialForm");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    fetch("https://script.google.com/macros/s/AKfycbxe8AkiOBWmDPUzfJEH_Zlw7MeiHkNTdHZXq6Iogcc6BV5SkhK2I8V7NhYiwjVqbJhP/exec", {
+      method: "POST",
+      body: new FormData(form)
+    })
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("response").innerText = "✅ Application submitted successfully!";
+      form.reset();
+    })
+    .catch(err => {
+      document.getElementById("response").innerText = "❌ Error submitting form!";
+      console.error(err);
+    });
+  });
